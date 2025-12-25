5aba8eedd33c3f78729f9e5b7b27d9fe69827933
nabila-azhari <152245697+nabila-azhari@users.noreply.github.com>
2025-12-26 00:56:45 +0700

    all api done, kurang miss kecil2 non api

--- frontend-user/src/api/profile.js
+++ frontend-user/src/api/profile.js
@@ -14,3 +14,15 @@ export const changePasswordAPI = async (payload) => {
   const res = await api.patch("/user/profile/password", payload);
   return res.data;
 };
+
+export const addExperienceAPI = (payload) => {
+  return api.post("/user/profile/experience", payload);
+};
+
+export const deleteExperienceAPI = (id) => {
+  return api.delete(`/user/profile/experience/${id}`);
+};
+
+export const editExperienceAPI = (id, payload) => {
+  return api.patch(`/user/profile/experience/${id}`, payload);
+};
